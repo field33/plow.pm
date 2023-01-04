499e2d164f5381b9f11db280e56e600951bc7ada
Maximilian Goisser <max@field33.com>
2023-01-04 13:51:10 +0100

    Fix frontmatter and add social share image
    CLOSES #5

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
 import {Footer} from "@components/Footer";
 import {PlowLogo} from "@components/PlowLogo";
 import {MatrixIcon} from "@components/MatrixIcon";
 
 const config: DocsThemeConfig = {
   useNextSeoProps() {
+    const { frontMatter } = useConfig()
     return {
-      titleTemplate: '%s – Plow'
+      titleTemplate: '%s – Plow',
+      description:
+          frontMatter.description || 'Plow: The ontology package manager',
+      openGraph: {
+        images: [
+          {
+            url: "/plow_social.png",
+            width: 588,
+            height: 308,
+          }
+        ]
+      }
     }
   },
   logo: <span className="flex items-center gap-2"><PlowLogo small primary/>Plow</span>,
